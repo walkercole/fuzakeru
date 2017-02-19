d348468a29c5319f383eb266c06ccfd2b450c3ab
Walker Cole <besidethebrave@gmail.com>
2017-02-18 23:56:39 -0500

    added first batch of css

--- index.js
+++ index.js
@@ -67,4 +67,154 @@ $(function() {
 		});
 	});
 
+});
+$(function() {
+	var animationName = 'animated tada';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button8').on('click',function() {
+		$('i.huge#move8').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated wobble';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button9').on('click',function() {
+		$('i.huge#move9').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated jello';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button10').on('click',function() {
+		$('i.huge#move10').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated bounceIn';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button11').on('click',function() {
+		$('i.huge#move11').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated bounceOut';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button12').on('click',function() {
+		$('i.huge#move12').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated bounceInDown';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button13').on('click',function() {
+		$('i.huge#move13').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated bounceInUp';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button14').on('click',function() {
+		$('i.huge#move14').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated bounceInLeft';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button15').on('click',function() {
+		$('i.huge#move15').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated bounceInRight';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button16').on('click',function() {
+		$('i.huge#move16').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated fadeIn';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button17').on('click',function() {
+		$('i.huge#move17').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated fadeOut';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button18').on('click',function() {
+		$('i.huge#move18').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated fadeInDown';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button19').on('click',function() {
+		$('i.huge#move19').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated fadeInUp';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button20').on('click',function() {
+		$('i.huge#move20').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated fadeInLeft';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button21').on('click',function() {
+		$('i.huge#move21').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
+});	
+$(function() {
+	var animationName = 'animated fadeInRight';
+	var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$('div.ui#button22').on('click',function() {
+		$('i.huge#move22').addClass(animationName).one(animationEnd,function() {
+			$(this).removeClass(animationName);
+		});
+	});
+
 });	
